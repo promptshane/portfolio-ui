40b4567215b47431e4abb9e7f08745b2e42b50ac
Casey Shane <caseyshane28@gmail.com>
2025-11-22 22:49:17 -0500

    Exclude local data/ftv/prisma/sqlite from output tracing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingExcludes: {
+    "*": [
+      "./data/**",
+      "./public/ftv/**",
+      "./prisma/**",
+      "./sqlite/**",
+    ],
+  },
 };
 
 export default nextConfig;
